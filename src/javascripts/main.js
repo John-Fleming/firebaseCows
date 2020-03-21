8647948e4a4b300cc6eb68f80ec9519b8a86e68e
John Fleming <johndeclanfleming@gmail.com>
2020-03-21 10:53:24 -0500

    added login button with firebase auth

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,11 @@
-import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import auth from './components/auth/auth';
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+};
+
+init();
